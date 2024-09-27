7479bbed97b9ea326c63c43824c9852eec2df3a5
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2024-09-26 21:15:36 -0300

    add fuentes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Ajustado para usar el prefijo './'
+import "./jquery.min"
+import "./browser.min"
+import "./breakpoints.min"
+import "./main"
