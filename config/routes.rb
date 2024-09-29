bde9d8097380dbede473e59f260196c6db763abc
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2024-09-29 17:26:39 -0300

    correccion final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
-  root "pages#index"
+  root "infos#about"
   get "/index", to: "pages#index"
   get "infos/about", to: "infos#about"
 
